b537b7498aabf7ce166a27ab6262e69e1d9f100f
BohdanBuhrii <bobuhrii@gmail.com>
2019-05-10 21:58:36 +0300

    added helper

---
+++ Helper/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+    public static class StringHelper
+    {
+        public static string[] Split(string str, char separator)
+        {
+            while (str.IndexOf(separator.ToString()+ separator.ToString()) > 0)
+            {
+                str = str.Replace(separator.ToString() + separator.ToString(), separator.ToString());
+            }
+            return str.Split(separator);
+        }
+    }
+}
